232e8c5e4355c49e683eeabb2adc917a6f79e09a
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-05-19 12:50:25 +0200

    Added woker node components back to repo

---
+++ worker_node_augmentations/ml_data_manager/blueprints/binaries.py
@@ -0,0 +1,34 @@
+import pathlib
+from datetime import datetime
+from http import HTTPStatus
+
+import flask
+import flask_openapi3
+
+CONTENT_TYPE = "application/octet-stream"
+ML_DATA_VOLUME = pathlib.Path("ml_data_volume")
+
+binaries_blp = flask_openapi3.APIBlueprint(
+    "binaries",
+    __name__,
+    url_prefix="/api/data/binaries",
+)
+
+
+@binaries_blp.post("/")
+def post_binary_data():
+
+    if flask.request.headers.get("Content-Type") != CONTENT_TYPE:
+        return {
+            "error": f"Bad request, the Header Content-Type should be '{CONTENT_TYPE}'"
+        }, HTTPStatus.BAD_REQUEST
+
+    # Get the binary data from the request
+    binary_data = flask.request.data
+
+    # Write the binary data to a file
+    current_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+    with open(f"{ML_DATA_VOLUME / current_time}.bin", "wb") as file:
+        file.write(binary_data)
+
+    return {"message": "Data posted successfully"}, HTTPStatus.OK
