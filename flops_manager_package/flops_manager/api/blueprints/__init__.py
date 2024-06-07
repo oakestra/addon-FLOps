503f3979c5304c6ea9eb5a36620de55d80d5ffab
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-06-07 11:33:11 +0200

    Add FLOPS tracking API call support

--- flops_manager_package/flops_manager/api/blueprints/__init__.py
+++ flops_manager_package/flops_manager/api/blueprints/__init__.py
@@ -1,4 +1,5 @@
 from .mock_data_provider import mock_data_provider_blp
 from .projects import projects_blp
+from .tracking import tracking_blp
 
-blueprints = [projects_blp, mock_data_provider_blp]
+blueprints = [projects_blp, mock_data_provider_blp, tracking_blp]
---
+++ flops_manager_package/flops_manager/api/blueprints/tracking.py
@@ -0,0 +1,31 @@
+from http import HTTPStatus
+from typing import Tuple
+
+import flask
+import flask_openapi3
+from flops_manager.classes.services.observatory.tracking_server.management import (
+    get_tracking_server,
+)
+from flops_manager.utils.exceptions.main import FLOpsManagerException
+from flops_utils.logging import colorful_logger as logger
+
+tracking_blp = flask_openapi3.APIBlueprint(
+    "tracking",
+    __name__,
+    url_prefix="/api/flops/tracking",
+)
+
+
+@tracking_blp.get("/")
+def get_tracking() -> Tuple[dict, HTTPStatus]:
+    try:
+        tracking_server_url = get_tracking_server(flask.request.json["customerID"]).get_url()
+    except FLOpsManagerException as e:
+        e.log()
+        return {"message": e.message}, e.http_status or HTTPStatus.INTERNAL_SERVER_ERROR
+    except Exception as e:
+        err_msg = "Unexpected exception occurred"
+        logger.exception(err_msg)
+        return {"message": f"{err_msg}:{e}"}, HTTPStatus.INTERNAL_SERVER_ERROR
+
+    return {"tracking_server_url": tracking_server_url}, HTTPStatus.OK
