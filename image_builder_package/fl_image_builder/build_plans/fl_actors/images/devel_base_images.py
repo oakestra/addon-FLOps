c61df43e38f88c206573d6c595cdd9e2aac0fa02
Malyuk-A <malyuk.alexander1999@gmail.com>
2025-06-20 09:32:08 +0000

    Comment in outdated examples

--- image_builder_package/fl_image_builder/build_plans/fl_actors/images/devel_base_images.py
+++ image_builder_package/fl_image_builder/build_plans/fl_actors/images/devel_base_images.py
@@ -1,10 +1,13 @@
 # NOTE: We need a separate var for the repo due to max line length linting.
+# TODO: These examples require migration to the Oakestra GitHub organisation.
 _mnist_sklearn_ml_repo = "https://github.com/Malyuk-A/flops_ml_repo_mnist_sklearn"
-_cifar10_keras_ml_repo = "https://github.com/Malyuk-A/flops_ml_repo_cifar10_keras"
-_cifar10_pytorch_ml_repo = "https://github.com/Malyuk-A/flops_ml_repo_cifar10_pytorch"
+# TODO: These examples are outdated and require overhauls due to updated dependencies.
+# _cifar10_keras_ml_repo = "https://github.com/<...>/flops_ml_repo_cifar10_keras"
+# _cifar10_pytorch_ml_repo = "https://github.com/<...>/flops_ml_repo_cifar10_pytorch"
 
 DEVEL_BASE_IMAGES_MAPPING = {
     _mnist_sklearn_ml_repo: "ghcr.io/oakestra/addon-flops/baseimage-mnist-sklearn:latest",
-    _cifar10_keras_ml_repo: "ghcr.io/oakestra/addon-flops/baseimage-cifar10-keras:latest",
-    _cifar10_pytorch_ml_repo: "ghcr.io/oakestra/addon-flops/baseimage-cifar10-pytorch:latest",
+    # TODO: These examples are outdated and require overhauls due to updated dependencies.
+    # _cifar10_keras_ml_repo: "ghcr.io/oakestra/addon-flops/baseimage-cifar10-keras:latest",
+    # _cifar10_pytorch_ml_repo: "ghcr.io/oakestra/addon-flops/baseimage-cifar10-pytorch:latest",
 }
